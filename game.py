367fe4fc541437fb2f13fab25d9ee935c36e7ef2
johannesmoskvil <johannesmos@hotmail.com>
2019-02-18 18:33:24 +0100

    Get next gamestates for each possible move

--- game.py
+++ game.py
@@ -15,7 +15,7 @@ class Game:
         self.clock = pygame.time.Clock()
         self.gamestate = initialize_game_from_file(level)
         self.done = False
-        self.animate = animate
+        self.shouldAnimate = animate
         pygame.time.set_timer(MOVE_GHOST_EVENT, 400)
         pygame.time.set_timer(PACMAN_TICK, 400)
 
@@ -27,6 +27,21 @@ class Game:
         for ghost in self.gamestate.ghosts:
             ghost.tick()
 
+    def animate(self):
+        # Wipe screen from previous cycle
+        self.screen.fill((255, 255, 255))
+
+        # Draw current gamestate to the screen
+        b.draw_board(self.gamestate, self.screen)
+        b.draw_lives(self.gamestate, self.screen)
+        b.draw_score(self.gamestate, self.screen)
+
+        pygame.display.flip()
+
+    def handle_input_action(self, event):
+        move = map_key_to_move(event)
+        self.gamestate.pacman.set_move(move)
+
     def execute_game_loop(self):
         # Handle keyboard events for manual playing
         for event in pygame.event.get():
@@ -36,18 +51,14 @@ class Game:
                 self.move_ghosts()
             if event.type == PACMAN_TICK:
                 self.gamestate.pacman.tick()
-            move = map_key_to_move(event)
-            self.gamestate.pacman.set_move(move)
+
+            if self.shouldAnimate:
+                self.handle_input_action(event)
+
             gamelogic.check_collisions(self.gamestate)
 
-        # Wipe screen from previous cycle
-        self.screen.fill((255, 255, 255))
-
-        # Draw current gamestate to the screen
-        b.draw_board(self.gamestate, self.screen)
-        b.draw_lives(self.gamestate, self.screen)
-        b.draw_score(self.gamestate, self.screen)
+        if self.shouldAnimate:
+            self.animate()
 
         # Limit FPS to 60 (still unnecessarily high)
         self.clock.tick(60)
-        pygame.display.flip()
--- gamelogic.py
+++ gamelogic.py
@@ -1,3 +1,7 @@
+import copy
+import utils.moves as moves
+
+
 def add_move_to_position(old_position, move):
     return old_position[0] + move[0], old_position[1] + move[1]
 
@@ -25,10 +29,29 @@ def check_collisions(gamestate):
                 reset(gamestate)
 
 
-# Returns how the gamestate would look if current move is executed
-def get_next_gamestate(gamestate, move):
+def get_next_gamestate_from_move(gamestate, move):
+    gamestate_copy = copy.deepcopy(gamestate)
+    gamestate_copy.pacman.set_move(move)
+    gamestate_copy.pacman.tick()
+    for ghost in gamestate_copy.ghosts:
+        ghost.tick()
+    return gamestate_copy
 
-    pass
+
+def get_next_gamestate_DEBUG(gamestate):
+    print("----------")
+    print(gamestate)
+    print('|')
+    print('|')
+    print('|')
+    print('\\/')
+    for move in moves.DIRECTION_FROM_MOVE.keys():
+        print(get_next_gamestate_from_move(move))
+
+
+# Returns how the gamestate would look if current move is executed
+def get_next_gamestate_by_move(gamestate):
+    return {move: get_next_gamestate_from_move(gamestate, move) for move in moves.DIRECTION_FROM_MOVE.keys()}
 
 
 def is_wall(gamestate, position):
